b678fc31c1e04998dc213596e102802cf110f3b4
Christopher Weir <christopherweir@Christophers-MacBook-Pro.local>
2014-06-11 14:23:44 +1000

    initial commit

---
+++ iln-countup.js
@@ -0,0 +1,75 @@
+'use strict';
+
+/*
+ *
+ *
+ *
+ *
+ *
+ */
+
+ApplicationConfiguration.registerModule('iln-countup');
+
+angular.module('iln-countup', []).
+    directive('ilnCountup', [
+        '$interval',
+        function ( $interval ) {
+        return {
+            restrict: 'E',
+            replace: true,
+            template: '<div id="iln-countup">{{ ilnTimeDifference }}</div>',
+            link:   function ( scope, elm, attrs ) {
+
+                scope.$watch( attrs.startTime, function ( value ) {
+                    // let's do nothing if the value comes in empty, null or undefined
+                    if ((value !== null) && (value !== undefined) && (value !== '')) {
+
+                        var startTime = value;
+                        var currentTime = new Date().getTime();
+                        var initialTime = currentTime - startTime;
+
+                        var convertTime = function( _milsec ){
+
+                            var _sec = _milsec / 1000;
+                            var _h = 0;
+                            var _m = 0;
+                            var _s = 0;
+
+                            _h = Math.floor(((_sec % 31536000) % 86400) / 3600);
+                            if( _h <= 9 ){
+                                _h = '0' + _h;
+                            }
+
+                            _m = Math.floor((((_sec % 31536000) % 86400) % 3600) / 60);
+                            if( _m <= 9 ){
+                                _m = '0' + _m;
+                            }
+
+                            _s = Math.floor(((_sec % 31536000) % 86400) % 3600) % 60;
+                            if( _s <= 9 ){
+                                _s = '0' + _s;
+                            }
+
+                            return _h +':'+ _m +':'+ _s;
+                        };
+                        // set initial time
+                        scope.ilnTimeDifference = convertTime( initialTime );
+
+                        var timerCountup = $interval(function(){
+                            initialTime = initialTime + 1000;
+                            scope.ilnTimeDifference = convertTime( initialTime );
+                        }, 1000);
+
+                        // kill $interval
+                        scope.$on(
+                            '$destroy',
+                            function( event ) {
+                                $interval.cancel( timerCountup );
+                            }
+                        );
+
+                     }
+                });
+            }
+        };
+    }]);
